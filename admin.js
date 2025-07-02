da8d471d204b96088848236d99485551c5ee4499
MF <bertiwesley15@gmail.com>
2025-07-01 23:59:47 -0300

    admin.js

--- admin.js
+++ admin.js
@@ -2,13 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebas
 import { getFirestore, collection, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBKtKrRP4AjLHcdeEdyTlTZC9hHdv5y7no",
+  authDomain: "metodo-forja.firebaseapp.com",
+  databaseURL: "https://metodo-forja-default-rtdb.firebaseio.com",
+  projectId: "metodo-forja",
+  storageBucket: "metodo-forja.firebasestorage.app",
+  messagingSenderId: "949719386034",
+  appId: "1:949719386034:web:5c015b5fbe082821ea86ab",
+  measurementId: "G-6Z9EJLY5LZ"
 };
 
 const app = initializeApp(firebaseConfig);
